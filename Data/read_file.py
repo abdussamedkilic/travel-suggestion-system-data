4679a080cfca8a3db48ba23c8b7bd1005a749d19
AbdussamedKILIC <62799218+AbdussamedKILIC@users.noreply.github.com>
2022-05-12 19:17:05 +0300

    use try catch

--- Data/read_file.py
+++ Data/read_file.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import heapq
 
 
@@ -17,28 +16,31 @@ class ReadFile:
         )  # return type is Data Frame
 
     def read_excel_for_flask(self, filename, city_name, place_name):
-        city_data = pd.read_excel(
-            self.file_path + filename, sheet_name=city_name
-        ).values.tolist()
-        place_index = None
-        for index, city in enumerate(city_data):
-            if city[0].lower().split(" ") == place_name.lower().split(
-                " "
-            ) or place_name.lower().__eq__(city_data[0][0].lower()):
-                place_index = index
-        if place_index:
-            place_data = city_data[place_index][1:]
-            top_ten_indices = heapq.nlargest(
-                10, range(len(place_data)), place_data.__getitem__
-            )
-            top_ten_names = []
-            for idx in top_ten_indices:
-                top_ten_names.append(city_data[idx][0])
-            return {
-                "success": True,
-                "place_name": place_name,
-                "top_ten_indices": top_ten_indices,
-                "top_ten_names": top_ten_names,
-                "place_data": place_data,
-            }
-        return {"success": False}
+        try:
+            city_data = pd.read_excel(
+                self.file_path + filename, sheet_name=city_name
+            ).values.tolist()
+            place_index = None
+            for index, city in enumerate(city_data):
+                if city[0].lower().split(" ") == place_name.lower().split(
+                    " "
+                ) or place_name.lower().__eq__(city_data[0][0].lower()):
+                    place_index = index
+            if place_index:
+                place_data = city_data[place_index][1:]
+                top_ten_indices = heapq.nlargest(
+                    10, range(len(place_data)), place_data.__getitem__
+                )
+                top_ten_names = []
+                for idx in top_ten_indices:
+                    top_ten_names.append(city_data[idx][0])
+                return {
+                    "success": True,
+                    "place_name": place_name,
+                    "top_ten_indices": top_ten_indices,
+                    "top_ten_names": top_ten_names,
+                    "place_data": place_data,
+                }
+            return {"success": False}
+        except Exception:
+            return {"success": False}
